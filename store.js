b4c089368d1670a2294ec1e44b9a919a4c8e5fa7
diego <diegotaveira1996@gmail.com>
2022-11-05 22:41:19 -0300

    store

--- store.js
+++ store.js
@@ -1,8 +1,2 @@
-let click = document.querySelector('.click')
-let tolt = document.querySelector('.toltip')
-
-click.addEventListener('click', ()=>{ 
-       tolt.classList.toggle('active')  
-})
 
 
